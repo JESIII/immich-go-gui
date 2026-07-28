36a10c71e67df1949deb90fd1d128b227a6f6a8e
Shitanshu Shivam <shivam.shitanshu2017@gmail.com>
2026-07-29 00:02:33 +0530

    refactor: extract custom widgets to gui/widgets/

---
+++ gui/widgets/__init__.py
@@ -0,0 +1,22 @@
+from .advanced_flag_row import AdvancedFlagRow
+from .base_page import BasePage
+from .cards import Card, FormSection
+from .droppable import DroppableLineEdit, DroppablePlainTextEdit
+from .eliding_label import ElidingLabel
+from .navigation import NavGroup, NavItem
+from .status_card import StatusCard
+from .switch_button import SwitchButton
+
+__all__ = [
+    "DroppableLineEdit",
+    "DroppablePlainTextEdit",
+    "SwitchButton",
+    "AdvancedFlagRow",
+    "Card",
+    "FormSection",
+    "ElidingLabel",
+    "BasePage",
+    "NavItem",
+    "NavGroup",
+    "StatusCard",
+]
